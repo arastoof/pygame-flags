63637bc7068569c79a6a1f53ec356090ca34349e
arastoof <arastoof@hotmail.com>
2024-09-02 05:25:44 +0100

    Added smoothing
    Using the smoothscale function, I added smoothing.
    This  can be toggled on and off by pressing the
    'a' key.
    
    Also stopped the star from being hardcoded.

--- Flags/Turkey.py
+++ Flags/Turkey.py
@@ -28,6 +28,8 @@ def rotatedStarPlotter(xCentre, yCentre, r, angle):
 # Screen resolution
 screenWidth = 1280
 screenHeight = 720
+smoothFactor = 4
+smoothing = True
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 
 # Define colours
@@ -35,35 +37,48 @@ red = (227, 10, 23) # Red represents the bloodshed of soldiers fighting for Turk
 white = (255, 255, 255) # White represents the diversity of Turkish cultures
 
 def drawFlag():
-    surface = pygame.Surface((screenWidth, screenHeight))
+    surfaceHeight = screenHeight * smoothFactor
+    surfaceWidth = screenWidth * smoothFactor
+    surface = pygame.Surface((surfaceWidth, surfaceHeight))
+
     # The Turkish flag has no official meaning, but there are many theories
     surface.fill(red)
 
     # According to legend, the image of the Turkish flag was formed through the reflection of the moon and the star in the blood of martyrs
-    moonRadius = screenHeight / 3
-    crescentRadius = screenHeight * 4 / 15
-    pygame.draw.circle(surface, white, (screenWidth / 2 - moonRadius, screenHeight / 2), moonRadius)
-    pygame.draw.circle(surface, red, (screenWidth / 2 - (crescentRadius * 0.9), screenHeight / 2), crescentRadius)
+    moonRadius = surfaceHeight / 3
+    crescentRadius = surfaceHeight * 4 / 15
+    pygame.draw.circle(surface, white, (surfaceWidth / 2 - moonRadius, surfaceHeight / 2), moonRadius)
+    pygame.draw.circle(surface, red, (surfaceWidth / 2 - (crescentRadius * 0.9), surfaceHeight / 2), crescentRadius)
 
-    starSize = 100
-    pygame.draw.polygon(surface, white, rotatedStarPlotter(screenWidth / 2 + starSize / 2, screenHeight / 2, starSize, math.radians(50)))
+    starSize = surfaceHeight / 7
+    pygame.draw.polygon(surface, white, rotatedStarPlotter(surfaceWidth / 2 + starSize / 2, surfaceHeight / 2, starSize, math.radians(50)))
 
+    surface = pygame.transform.smoothscale(surface, (screenWidth, screenHeight))
     return surface
 
 if __name__ == "__main__":
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    screen = pygame.display.set_mode((screenWidth, screenHeight))
     running = True
 
     surface = drawFlag()
-    screen.blit(surface, (0, 0))
-    pygame.display.update()
 
     clock = pygame.time.Clock()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    smoothing = not smoothing
+                    if smoothing:
+                        smoothFactor = 4
+                    else:
+                        smoothFactor = 1
+                    surface = drawFlag()
+
+        screen.blit(surface, (0, 0))
+        pygame.display.update()
         clock.tick(5)
 
     pygame.quit()
