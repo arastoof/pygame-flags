9b33029786864225f38759b75673a16a8fb460b8
arastoof <arastoof@hotmail.com>
2024-09-02 05:59:20 +0100

    Added smoothing
    Using the smoothscale function. This can be
    toggled using the 'a' key.
    Also there was a bit of white at the bottom
    that I fixed, but the fix is kinda trash but IDC.

--- Flags/USA.py
+++ Flags/USA.py
@@ -20,6 +20,8 @@ def starPlotter(x, y, r):
 # Screen resolution
 screenWidth = 1280
 screenHeight = 720
+smoothFactor = 4
+smoothing = True
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 
 # Define colours
@@ -28,14 +30,17 @@ white = (255, 255, 255) # White represents purity and innocence
 blue = (60, 59, 110) # Blue represents vigilance, perseverance and justice
 
 def drawFlag():
-    surface = pygame.Surface((screenWidth, screenHeight))
+    surfaceHeight = screenHeight * smoothFactor
+    surfaceWidth = screenWidth * smoothFactor
+    surface = pygame.Surface((surfaceWidth, surfaceHeight))
 
     # Generates the stripes. 13 stripes represent 13 original colonies.
     surface.fill(white)
     for counter in range (0, 13, 2):
-        pygame.draw.rect(surface, red, [0, (screenHeight / 13) * counter, screenWidth, (screenHeight / 13)])
+        pygame.draw.rect(surface, red, [0, (surfaceHeight / 13) * counter, surfaceWidth, (surfaceHeight / 13)])
+    pygame.draw.rect(surface, red, [0, (surfaceHeight / 14) * 13, surfaceWidth, (surfaceHeight / 13)])
     
-    canton = (screenWidth * 4 / 9, screenHeight * 7 / 13) # Size of the blue part
+    canton = (surfaceWidth * 4 / 9, surfaceHeight * 7 / 13) # Size of the blue part
     pygame.draw.rect(surface, blue, [0, 0, canton[0], canton[1]]) # Draws the blue part of the flag
 
     # Draws the 50 stars on the blue part of the flag, representing each of the states in the canton
@@ -47,23 +52,33 @@ def drawFlag():
         if i % 2 == 0:
             for k in range (2, 12, 2):
                 pygame.draw.polygon(surface, white, starPlotter(k * 1.22 * starSize, starSize * i, starSize / 2))
-
+    
+    surface = pygame.transform.smoothscale(surface, (screenWidth, screenHeight))
     return surface
 
 if __name__ == "__main__":
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    screen = pygame.display.set_mode((screenWidth, screenHeight))
     running = True
 
     surface = drawFlag()
-    screen.blit(surface, (0, 0))
-    pygame.display.update()
 
     clock = pygame.time.Clock()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    smoothing = not smoothing
+                    if smoothing:
+                        smoothFactor = 4
+                    else:
+                        smoothFactor = 1
+                    surface = drawFlag()
+
+        screen.blit(surface, (0, 0))
+        pygame.display.update()
         clock.tick(5)
 
     pygame.quit()
